fa5f0388b42b00df6f4be6899ca1dce7f8d7bc09
Denusha2010 <134531354+Denusha2010@users.noreply.github.com>
2023-06-05 10:48:13 +0530

    Add files via upload

---
+++ python 1.py
@@ -0,0 +1,16 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: C:/Users/Suthakaran/Desktop/python 1.py ===============
+Hello, World!
+>>> 
+=============== RESTART: C:/Users/Suthakaran/Desktop/python 1.py ===============
+Hello, World!
+What is your name?Denusha
+Hello Denusha
+>>> Weight=50
+>>> print(Weight)
+50
+>>> Height=158
+>>> print(Height)
+158
